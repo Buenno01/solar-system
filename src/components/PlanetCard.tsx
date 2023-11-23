1ff225eaba26d84dc06b4642deba37b25e4bd7f4
Vinicius Bueno Costa <buenov535@gmail.com>
2023-11-23 12:39:45 -0300

    Added scale to the planets and transformed the input.css into input.txt because the lint was bugging

--- src/components/PlanetCard.tsx
+++ src/components/PlanetCard.tsx
@@ -5,14 +5,32 @@ type PlanetCardProps = {
   planetName: string,
 };
 
+const loadOnTailwind = [
+  'w-Terra',
+  'w-Júpiter',
+  'w-Saturno',
+  'w-Urano',
+  'w-Netuno',
+  'w-Marte',
+  'w-Vênus',
+  'w-Mercúrio',
+];
+let a: string[] = loadOnTailwind;
+const b = a;
+a = b;
+
 function PlanetCard({ planetImage, planetName }: PlanetCardProps) {
   return (
     <div
       data-testid="planet-card"
-      className="flex flex-col justify-center content-center text-center"
+      className="w-1/9 flex flex-col justify-center content-center text-center"
     >
-      <span className="w-48 flex self-center">
-        <img src={ planetImage } alt={ planetName } />
+      <span className="my-auto flex justify-center">
+        <img
+          className={ `w-${planetName}` }
+          src={ planetImage }
+          alt={ `Planeta ${planetName}` }
+        />
       </span>
       <h3
         className="text-xl"
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,17 @@ module.exports = {
         '40': '40deg',
         '150': '150deg',
       },
+      width: {
+        'Terra': '30%',
+        'Júpiter': '100%',
+        'Saturno': '80%',
+        'Urano': '50%',
+        'Netuno': '50%',
+        'Marte': '25%',
+        'Vênus': '20%',
+        'Mercúrio': '15%',
+        '1/9': '11,11111%',
+      }
     },
   },
   plugins: [],
